c67c1923267e19314c58689aba42eedb0efa6663
Rafael Sant Anna <88406174+Rafael-Sant-Anna@users.noreply.github.com>
2023-07-03 08:31:51 -0300

    Functions

---
+++ Modulo 10 - JavaScript Básico/15 - Functions/javascript.js
@@ -0,0 +1,13 @@
+
+
+function semParametro (){
+    console.log('Sem parametro')    
+}
+
+function com1_Parametro (parametro){
+    console.log(parametro)
+}
+
+function com2_Parametros (nome, mensagem){
+    console.log(`Olá ${nome}, seja muito bem vindo! ${mensagem}`)
+}
---
+++ Modulo 10 - JavaScript Básico/16 - Functions Return/javascript.js
@@ -0,0 +1,10 @@
+
+
+function convertMilhasEmKm (milhas) {
+    let km = milhas * 1.6
+
+    return km
+}
+
+let resultado = convertMilhasEmKm(100);
+console.log(resultado)
