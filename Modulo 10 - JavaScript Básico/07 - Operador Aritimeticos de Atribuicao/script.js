d107ca1b26c32e3a0585d0db30b47913743a399a
Rafael Sant Anna <88406174+Rafael-Sant-Anna@users.noreply.github.com>
2023-07-03 08:25:02 -0300

    Operadora

---
+++ Modulo 10 - JavaScript Básico/07 - Operador Aritimeticos de Atribuicao/script.js
@@ -0,0 +1,55 @@
+//Atribução
+let a = 10;
+let b =5;
+let c = a +b;
+
+console.log(a);
+console.log(b);
+console.log(`Adição ${c}`);
+
+//subtração
+let d = 10;
+let e =5;
+let f = a - b;
+
+console.log(d);
+console.log(e);
+console.log(`Subtração ${f}`);
+
+//Multiplicação
+let g = 10;
+let h = 5;
+let i = a * b;
+
+console.log(g);
+console.log(h);
+console.log(`Multiplicação ${i}`);
+
+//Divisão
+let j = 10;
+let l =5;
+let m = a / b;
+
+console.log(j);
+console.log(l);
+console.log(`Divisão ${m}`);
+
+//Incremento
+let n = 0 ;
+n++;
+console.log(`Incremento ${n}`);
+
+//Decremento
+let O = 0 ;
+O--;
+console.log(`Decremento ${O}`);
+
+//Atribuição
+let p = 12;
+p+= 25
+console.log(`Atribuição += ${p}`);
+
+let q = 25;
+a+= 12
+console.log(`Atribuição -= ${q}`);
+
